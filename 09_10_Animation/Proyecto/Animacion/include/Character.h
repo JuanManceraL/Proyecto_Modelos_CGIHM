ab4e7478b62fac545204594faf7f880457c96345
Ian Alexis Solis Hernández <ianalexis9137@gmail.com>
2025-05-08 23:35:43 -0600

    Añadiendo modelos y cabeceras para ejedrez
    Se añade gitignore para archivos raros de vs code y se añaden los obj para el tablero de ajedrez.

---
+++ 09_10_Animation/Proyecto/Animacion/include/Character.h
@@ -0,0 +1,30 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+#include <glm/glm.hpp>
+
+/*
+* Estructura de datos que guarda informacion de los personajes (piezas)
+* name --> Nombre de la pieza (Prints en consola)
+* position --> Puntero a la posicion de la pieza en el mundo
+* team  --> Indica de que equipo es la pieza. Valores:
+*           1 --> Equipo Blancas
+*           2 --> Equipo Negras
+* type --> Tipo de pieza que represaenta el personaje:
+*           1 --> Torre
+*           2 --> Caballo
+*           3 --> Alfil
+*           4 --> Reyna
+*           5 --> Rey
+*           6 --> Peon
+*/
+struct Character {
+    std::string name;
+    glm::vec3* position;
+    unsigned int team;
+    unsigned int type;
+    unsigned int alive;
+};
+
+#endif // CHARACTER_H
